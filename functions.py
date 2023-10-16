926042b45f7344171287c055152abb3e21d66aab
Olena Abert <oabert@gmail.com>
2023-10-16 12:45:22 -0400

    Add filepath as constant var, add locked message

--- functions.py
+++ functions.py
@@ -1,9 +1,16 @@
-def read_todos(filepath='todos_list'):
+FILEPATH = 'todos_list'
+
+
+def read_todos(filepath=FILEPATH):
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_input, filepath='todos_list'):
+def write_todos(todos_input, filepath=FILEPATH):
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_input)
+
+
+if __name__ == "__main__":
+    print("Hello, I am locked in functions.py message")
