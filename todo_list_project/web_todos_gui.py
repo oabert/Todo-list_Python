c25205176031a9ad0d721c4ac0264c329c972bf1
Olena Abert <oabert@gmail.com>
2023-10-25 09:01:49 -0400

    Add new todo function to webz app

--- todo_list_project/web_todos_gui.py
+++ todo_list_project/web_todos_gui.py
@@ -1,8 +1,17 @@
 import streamlit as st
 
-from functions import get_read_todos
+from functions import get_read_todos, write_todos
 
 todos_list = get_read_todos()
+
+
+def add_todo():
+    todo_input = st.session_state['new_todo'] + '\n'
+    # """Updated todo list with .append"""
+    todos_list.append(todo_input)
+    # """write/add new todo to the list/update todos_list.txt"""
+    write_todos(todos_list)
+
 
 st.title('My todo app')
 st.subheader('This is my todolist project')
@@ -11,6 +20,7 @@ st.write('This app will increase your productivity')
 for todo_item in todos_list:
     st.checkbox(todo_item)
 
-st.text_input(label='', placeholder='Add your todo...')
+st.text_input(label='', placeholder='Add your todo...',
+              on_change=add_todo, key='new_todo')
 
 # print('Hello')
